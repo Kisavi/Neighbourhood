350b823f8523ee5b1ee629b67b3e6da6957f2056
Kisavi <deniskisavi@gmail.com>
2022-06-17 03:11:46 +0300

    define urls that will call the view functions for displaying the index and home page

---
+++ Neighbourhood/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path, include
+
+from Neighbourhood import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home', views.home, name='home'),
+
+]
