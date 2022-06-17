ba70cac9b186648c7d175d65e7315d49f3d3e801
Kisavi <deniskisavi@gmail.com>
2022-06-17 14:54:05 +0300

    register urls to call the views to display the post details and profile section

--- Neighbourhood/urls.py
+++ Neighbourhood/urls.py
@@ -6,5 +6,7 @@ from Neighbourhood import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
+    path('view-post', views.home, name='view-post'),
+    path('post-details', views.home, name='post-details'),
 
 ]
