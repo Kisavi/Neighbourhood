d5d7e0d22a8493244cc42201040bf1d95f30d915
Kisavi <deniskisavi@gmail.com>
2022-06-19 00:06:11 +0300

    add hood column in the profile table

--- Neighbourhood/models.py
+++ Neighbourhood/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
+HOODS = (
+    ('Kahawa Sukari', 'Kahawa Sukari'),
+    ('Kahawa West', 'Kahawa West'),
+    ('Kasarani', 'Kasarani')
+)
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,6 +15,7 @@ class Profile(models.Model):
     bio = models.TextField(max_length=300)
     photo = CloudinaryField('image',
                             default='http://res.cloudinary.com/dim8pysls/image/upload/v1639001486/x3mgnqmbi73lten4ewzv.png')
+    hood = models.CharField(choices=HOODS, max_length=25, default=1)
 
     def __str__(self):
         return self.user.username
