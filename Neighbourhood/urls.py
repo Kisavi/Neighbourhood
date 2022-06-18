52be2d363cc0ad9d102553302985d8bf6ac32cd7
Kisavi <deniskisavi@gmail.com>
2022-06-19 00:45:00 +0300

    map the profile view function with the url to redirect the user to the profile page

--- Neighbourhood/urls.py
+++ Neighbourhood/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
     path('view-post', views.view_post, name='view-post'),
-    path('profile', views.profile, name='profile'),
+    path('update-profile', views.profile, name='profile'),
     path('signup', SignupView.as_view(), name='signup'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
+    path('hood-profile', views.hoodProfile, name='hood-profile')
 
 
 ]
