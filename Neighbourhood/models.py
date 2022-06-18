b0065a82fc56f46a7cf6098b521131f41b6cc59e
Kisavi <deniskisavi@gmail.com>
2022-06-18 03:03:41 +0300

    create profile model for a registered user

--- Neighbourhood/models.py
+++ Neighbourhood/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True)
+    bio = models.TextField(max_length=300)
+    photo = CloudinaryField('image',
+                            default='http://res.cloudinary.com/dim8pysls/image/upload/v1639001486/x3mgnqmbi73lten4ewzv.png')
+
+    def __str__(self):
+        return self.user.username
