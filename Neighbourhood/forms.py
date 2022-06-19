969a195f55fcfcb9f58e0b39e54e7fbf7a7f102f
Kisavi <deniskisavi@gmail.com>
2022-06-19 18:18:39 +0300

    create a commemnt form from the comment model to enable a user comment on a post

--- Neighbourhood/forms.py
+++ Neighbourhood/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import Profile
+from .models import Profile, Post, Comment
 
 
 class SignupForm(UserCreationForm):
@@ -91,3 +91,20 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['photo', 'bio', 'hood']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['description']
+
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment....'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
