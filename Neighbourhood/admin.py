93953a58d121586080c2300350c38ae81186b4c3
Kisavi <deniskisavi@gmail.com>
2022-06-20 13:01:56 +0300

    register Alert and Business model in the admin section

--- Neighbourhood/admin.py
+++ Neighbourhood/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Post
+from .models import Profile, Post, Business, Alert
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
+admin.site.register(Business)
+admin.site.register(Alert)
