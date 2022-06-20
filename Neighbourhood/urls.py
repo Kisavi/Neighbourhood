e1977d06b3ae152693e47f1baa0a026b8ac234c3
Kisavi <deniskisavi@gmail.com>
2022-06-20 14:56:32 +0300

    define the url which will trigger the search view function

--- Neighbourhood/urls.py
+++ Neighbourhood/urls.py
@@ -8,12 +8,13 @@ from .views import *
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
-    path('view-post', views.view_post, name='view-post'),
+    # path('view-post', views.view_post, name='view-post'),
     path('update-profile', views.update_profile, name='profile'),
     path('signup', SignupView.as_view(), name='signup'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     path('hood-profile', views.hoodProfile, name='hood-profile'),
     path('comment/<id>', views.comment, name='comment'),
+    path('search', SearchBusinessView.as_view(), name='search_business')
 
 
 ]
