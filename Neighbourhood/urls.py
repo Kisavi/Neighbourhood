8603af8e440cb01f077fec09795c2ab7a2a94525
Kisavi <deniskisavi@gmail.com>
2022-06-21 03:18:04 +0300

    add the star rating url to enable a user to leave ratings for a business

--- Neighbourhood/urls.py
+++ Neighbourhood/urls.py
@@ -8,13 +8,18 @@ from .views import *
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
-    # path('view-post', views.view_post, name='view-post'),
     path('update-profile', views.update_profile, name='profile'),
     path('signup', SignupView.as_view(), name='signup'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     path('hood-profile', views.hoodProfile, name='hood-profile'),
     path('comment/<id>', views.comment, name='comment'),
-    path('search', SearchBusinessView.as_view(), name='search_business')
+    path('search', SearchBusinessView.as_view(), name='search_business'),
+    path('welcome', views.welcome, name='welcome'),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    # path('alerts', views.alert, name='alerts'),
+    # path('posts', views.post, name='posts'),
+    # path('business', views.business, name='business'),
+
 
 
 ]
